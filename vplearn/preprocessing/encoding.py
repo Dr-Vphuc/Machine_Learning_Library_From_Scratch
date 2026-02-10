c1f5a0d4756aa4725ecb4e1705e0db60a19eee77
Dr-Vphuc <ngphuc689@gmail.com>
2026-02-10 17:17:03 +0700

    feat(preprocessing): add bag_of_word_encoding

--- vplearn/preprocessing/encoding.py
+++ vplearn/preprocessing/encoding.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 class Encoding:
     def __init__(self):
@@ -12,3 +13,30 @@ class Encoding:
             label_mapping[_class] = idx
             
         return y.map(label_mapping)
+
+    def _word_with_idx(self, v: set) -> dict:
+        result = {}
+        for idx, key in enumerate(v):
+            result[key] = idx
+        
+        return result
+
+    def bag_of_words_encoding(self, X: list[str], v: set, phrase: bool) -> pd.DataFrame:
+        vector_list = []
+        vector_size = len(v)
+        d = self._word_with_idx(v)
+        
+        if phrase:
+            for d in X:
+                tokens = d.strip().split(' ')
+                vector = np.zeros(vector_size)
+                idx_list = []
+                
+                for token in tokens:
+                    idx_list.append(d[token])
+                    
+                np.add.at(vector, idx_list, 1)
+                vector_list.append(vector)
+            return np.array(vector_list)
+        else:
+            raise TypeError('Data does not pharse processed is not support yet.')
