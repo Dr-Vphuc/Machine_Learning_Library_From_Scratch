50371894d360b5aee011b78c9704392a4138af1a
Dr-Vphuc <ngphuc689@gmail.com>
2026-02-11 16:51:19 +0700

    FIX(nb): fix check type error

--- vplearn/classification/naive_bayes/gaussian_nb.py
+++ vplearn/classification/naive_bayes/gaussian_nb.py
@@ -11,7 +11,7 @@ class GaussianNB(Model):
     def fit(self, X: pd.DataFrame, y: pd.Series) -> None:
         if not isinstance(X, pd.DataFrame):
             raise TypeError("X must be np.ndarray or pd.DataFrame")
-        if not isinstance(y, pd.Series) or not isinstance(y, pd.DataFrame):
+        if not isinstance(y, pd.Series) and not isinstance(y, pd.DataFrame):
             raise TypeError("y must be np.ndarray or pd.DataFrame/Series")
         self.mu_list, self.std_list, self.pi_list = self._compute_data_features(X, y)
         
