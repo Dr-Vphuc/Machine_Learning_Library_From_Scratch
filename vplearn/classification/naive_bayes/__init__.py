8d24fc77fd76fd5b3f07b620d3518ddf8e5a131e
Dr-Vphuc <ngphuc689@gmail.com>
2026-02-11 10:28:13 +0700

    feat(nb): add bernouli_nb to namespace

--- vplearn/classification/naive_bayes/__init__.py
+++ vplearn/classification/naive_bayes/__init__.py
@@ -1,4 +1,5 @@
 from .gaussian_nb import GaussianNB
 from .mutinomial_nb import MutinomialNB
+from .bernouli_nb import BernouliNB
 
-__all__ = ['GaussianNB', 'MutinomialNB']
+__all__ = ['GaussianNB', 'MutinomialNB', 'BernouliNB']
