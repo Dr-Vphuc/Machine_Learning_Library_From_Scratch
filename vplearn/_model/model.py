4c7f518ff29c2f148840f53d060e4cb691f316eb
Dr-Vphuc <ngphuc689@gmail.com>
2026-02-12 16:16:35 +0700

    FEAT(model): add input check

--- vplearn/_model/model.py
+++ vplearn/_model/model.py
@@ -1,9 +1,33 @@
+import pandas as pd
+import numpy as np
+
 class Model:
     def __init__(seft):
         pass
     
-    def fit(data):
+    def fit():
         pass
     
-    def predict(X):
+    def predict():
         pass
+    
+    def _check_fit_input_format(
+        self, 
+        X: pd.DataFrame | pd.Series | np.ndarray, 
+        y: pd.Series | np.ndarray
+    ) -> bool:
+        if not isinstance(X, pd.DataFrame | pd.Series | np.ndarray):
+            raise TypeError("X must be np.ndarray or pd.DataFrame/Series")
+        if not isinstance(y, pd.DataFrame | np.ndarray):
+            raise TypeError("y must be np.ndarray or pd.Series")
+        
+        return True
+    
+    def _check_predict_input_format(
+        self,
+        X: pd.DataFrame | pd.Series | np.ndarray
+    ) -> bool:
+        if not isinstance(X, pd.DataFrame | pd.Series | np.ndarray):
+            raise TypeError("X must be np.ndarray or pd.DataFrame/Series")
+        
+        return True
