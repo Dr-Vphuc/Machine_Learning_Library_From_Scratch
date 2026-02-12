e4b755dc39e3dbad09f2cc2cb8e4c9d793d897a2
Dr-Vphuc <ngphuc689@gmail.com>
2026-02-12 14:24:46 +0700

    TEST(testing): add testing module

---
+++ testing/bernouli_naive_bayes.py
@@ -0,0 +1,18 @@
+from vplearn.classification.naive_bayes import BernouliNB
+
+import pandas as pd
+
+x1 = (1, 1, 1, 0, 0, 0, 0, 0, 0)
+x2 = (1, 1, 0, 1, 1, 0, 0, 0, 0)
+x3 = (0, 1, 0, 0, 1, 1, 0, 0, 0)
+x4 = (0, 1, 0, 0, 0, 0, 1, 1, 1)
+
+X_train = pd.DataFrame([x1, x2, x3, x4])
+y_train = pd.Series(['B', 'B', "B", "N"])
+X_test = pd.DataFrame([1, 1, 0, 0, 0, 0, 0, 1, 1])  
+# X_test = pd.DataFrame([1, 0, 0, 1, 0, 0, 0, 1, 0])
+
+bnb = BernouliNB()
+bnb.fit(X_train, y_train)
+pred = bnb.predict(X_test)
+print(pred)
---
+++ testing/gausian_naive_bayes.py
@@ -0,0 +1,30 @@
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+from vplearn.classification.naive_bayes import GaussianNB
+from vplearn.preprocessing import Encoding
+
+data = sns.load_dataset('iris')
+train = pd.concat([data[:40], data[50:90], data[100:140]])
+test = pd.concat([data[40:50], data[90:100], data[140:150]])
+
+ec = Encoding()
+
+X_train = train.iloc[:,:-1]
+y_train = train.iloc[:,-1]
+y_train = ec.label_encoding(y_train)
+X_test = test.iloc[:,:-1]
+y_test = test.iloc[:,-1]
+y_test = ec.label_encoding(y_test)
+
+print(y_train)
+
+gnb = GaussianNB()
+gnb.fit(X_train, y_train)
+pred = gnb.predict(X_test)
+print(pred)
