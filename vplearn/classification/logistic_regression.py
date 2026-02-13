7eff15ba17a5337b2cc39237b133a35fb9c69842
Dr-Vphuc <ngphuc689@gmail.com>
2026-02-13 16:52:15 +0700

    FEAT(logistic): add softmax activate func regression

--- vplearn/classification/logistic_regression.py
+++ vplearn/classification/logistic_regression.py
@@ -4,6 +4,7 @@ from typing import Literal
 
 import pandas as pd
 import numpy as np
+from scipy import sparse
 
 class LogisticRegression(ModelBaseModel):
     def __init__(
@@ -13,8 +14,7 @@ class LogisticRegression(ModelBaseModel):
         max_count: int = 10000,
         tol: float = 1e-4,
         activate: Literal['auto', 'sigmoid', 'softmax']  = "auto",
-        threshold: float  = 0.5,
-        auto_label = bool
+        threshold: float  = 0.5
     ):
         super().__init__()
         self.eta = eta
@@ -25,7 +25,6 @@ class LogisticRegression(ModelBaseModel):
         if not (0 < threshold < 1):
             raise ValueError("threshold must be between 0 and 1")
         self.threshold = threshold
-        self.auto_label = auto_label
         
     
     def fit(self, X: pd.DataFrame, y: pd.Series) -> None:
@@ -41,24 +40,32 @@ class LogisticRegression(ModelBaseModel):
         
         if self.activate is None or self.activate == 'auto':
             if self.n_classes == 2:
+                self._activate_func = 'sigmoid'
                 self.coefficents = self._logistic_sigmoid_regression(X_train, y_train)
             elif self.n_classes > 2:
-                pass
+                self._activate_func = 'softmax'
+                self.w = self._softmax_regression(X_train, y_train)
         if self.activate == 'sigmoid':
             if self.n_classes > 2:
                 raise ValueError("Sigmoid activation is only used for binary classification.")
+            self._activate_func = 'sigmoid'
             self.coefficents = self._logistic_sigmoid_regression(X_train, y_train)
         elif self.activate == 'softmax':
-            pass
+            self._activate_func = 'softmax'
+            self.w = self._softmax_regression(X_train, y_train)
     
     def predict(self, X: pd.DataFrame):
         self._check_predict_input_format(X)
         
         X = self._correct_predict_input_format(X)
         
-        return self._predict_sigmoid_logistic_class(X)
+        return (
+            self._predict_sigmoid_logistic_class(X)
+            if self._activate_func == 'sigmoid'
+            else self._predict_softmax_logistic_class(self.w, X)
+            )
     
-    def _get_number_of_classes(self, y: pd.Series) -> int:
+    def _get_number_of_classes(self, y: np.ndarray) -> int:
         return len(np.unique(y).tolist())
     
     def _sigmoid(self, s):
@@ -96,3 +103,73 @@ class LogisticRegression(ModelBaseModel):
         
         self.confidents = confidents
         return np.array(preds)
+
+    def _convert_labels(self, y, C):
+        """
+        convert 1d label to a matrix label: each column of this
+        matrix coresponding to 1 element in y. In i-th column of Y,
+        only one non-zeros element located in the y[i]-th position,
+        and = 1 ex: y = [0, 2, 1, 0], and 3 classes then return
+
+        [[1, 0, 0, 1],
+        [0, 0, 1, 0],
+        [0, 1, 0, 0]]
+        """
+        Y = sparse.coo_matrix((np.ones_like(y),
+        (y, np.arange(len(y)))), shape = (C, len(y))).toarray()
+        return Y
+
+    def _softmax_stable(self, Z):
+        """
+        Compute softmax values for each sets of scores in Z.
+        each column of Z is a set of score.
+        """
+        e_Z = np.exp(Z - np.max(Z, axis = 0, keepdims = True))
+        A = e_Z / e_Z.sum(axis = 0)
+        return A
+
+    def _softmax(self, Z):
+        """
+        #Compute softmax values for each sets of scores in V.
+        #each column of V is a set of score.
+        """
+        e_Z = np.exp(Z)
+        A = e_Z / e_Z.sum(axis = 0)
+        return A
+
+    def _softmax_regression(self, X, y):
+        W = [self.w_init]
+        C = self._get_number_of_classes(y)
+        Y = self._convert_labels(y, C)
+        N = X.shape[0]
+        d = X.shape[1]
+
+        count = 0        
+        while count < self.max_count:            
+            mix_id = np.random.permutation(N)
+            for i in mix_id:
+                xi = X[i, :].reshape(d, 1)
+                yi = Y[:, i].reshape(C, 1)
+                ai = self._softmax(np.dot(W[-1].T, xi))
+                W_new = W[-1] + self.eta * xi.dot((yi - ai).T)
+                count += 1
+                if count % self.check_w_after == 0:
+                    if np.linalg.norm(W_new - W[-self.check_w_after]) < self.tol:
+                        return W[-1]
+                W.append(W_new)
+        return W[-1]
+
+    def _cost(self, X, Y, W):
+        A = self._softmax(W.T.dot(X))
+        return -np.sum(Y*np.log(A))
+
+    def _predict_softmax_logistic_class(self, w, X) -> np.ndarray:
+        """
+        predict output of each columns of X
+        Class of each x_i is determined by location of max probability
+        Note that class are indexed by [0, 1, 2, ...., C-1]
+        """
+        w = np.array(w)
+        X = X.T
+        A = self._softmax_stable(w.T.dot(X))
+        return np.argmax(A, axis = 0)
